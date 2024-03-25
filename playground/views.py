66c5e7349de09addd7b189728fb692028d677a42
Kim Jiwoo <88700207+jiwoo1087@users.noreply.github.com>
2024-03-25 12:16:41 +0900

    views -> templates에 context 전달

--- playground/views.py
+++ playground/views.py
@@ -5,7 +5,11 @@ def say_hello(request):
     return HttpResponse("Hello World");
 
 def say_hello_html(request):
-    return render(request, "playground/hello.html");
+    return render(request, "playground/hello.html", context={'name': '지우야','greeting':'안녀엉~'});
 def say_bye_html(request):
-    return render(request, "playground/bye.html");
+    context = {
+        'singer': '데이식스',
+        'title' : '해와 달 처럼',
+    }
+    return render(request, "playground/bye.html", context = context);
 
